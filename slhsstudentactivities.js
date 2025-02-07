9ab9a3f3b838a3f176115ca5478645363b62e1b4
JaceDaWon1 <realgoddesshere@gmail.com>
2025-02-07 15:03:27 -0800

    Add files via upload

---
+++ slhsstudentactivities.js
@@ -0,0 +1,7 @@
+$(“#hide-button”).click(function(){
+	$(“#nav-bar”).animate({“left”, “-100px”}, “slow”);
+});
+		
+$(“#show-button”).click(function(){
+	$(“#nav-bar”).animate({“left”, “100px”}, “slow”);
+});
